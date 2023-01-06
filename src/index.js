71e4767e8bb8604010692daeff5cc884dab5b1a4
makedonsky97 <dmytrofil99@gmail.com>
2023-01-06 03:01:37 +0200

    fix: add math.abs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+    n = (Math.abs(n)).toString();
+    let arr = [];
+    for(let i = 0; i< n.length; i++){
+        arr.unshift(n[i])
+    }
+    return +arr.join('')
+ }
